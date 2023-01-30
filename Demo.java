12b7ad8553b759ebc179b0115332e115d1e9ce60
Nurgali01 <nurgalishansahrov@gmail.com>
2023-01-31 03:11:40 +0600

    title metody quryldy

--- Demo.java
+++ Demo.java
@@ -4,4 +4,9 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Salem Alem");
     }
+
+}
+
+public static void title(){
+    System.out.println("TITLE")
 }
